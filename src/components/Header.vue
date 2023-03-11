725e30a80808ebeb89e2a0c14c2795fcdc7fc152
Timothy <timothyzhong2@gmail.com>
2023-03-10 19:48:47 -0800

    made code for app and header actually understandable

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,61 +1,69 @@
 <script setup></script>
 
 <template>
-  <div class="header">
-    <img
-      src="../assets/logo-placeholder-image.png"
-      alt="Logo"
-      width="50"
-      height="50"
-    />
+  <div class="container">
+    <div class="header">
+      <img
+        src="../assets/logo-placeholder-image.png"
+        alt="Logo"
+        width="50"
+        height="50"
+        class="image"
+      />
 
-    <span>Endangered Animals Tracker</span>
+      <span class="title">Endangered Animals Tracker</span>
+    </div>
 
-    <nav>
-      <a href="#">Settings</a>
-      <a href="#">About</a>
+    <nav class="nav-bar">
+      <a href="#" class="nav-link">Settings</a>
+      <a href="#" class="nav-link">About</a>
     </nav>
   </div>
 </template>
 
 <style scoped>
 /* CONTAINER */
-.header {
+.container {
   border-top: 10px solid #70c3ed;
   background-color: rgba(112, 195, 237, 0.5);
 
   display: flex;
   align-items: center;
+  justify-content: space-between;
 }
 
-/* LOGO */
-img {
-  margin: 2px 10px;
+/* HEADER */
+.header {
+  display: flex;
+  align-items: center;
+  gap: 10px;
 }
 
-/* TITLE */
-span {
+.image {
+  margin-left: 5px;
+}
+
+.title {
   font-size: 26px;
   font-weight: bold;
 }
 
 /* NAVIGATION */
-nav {
-  order: 0;
-  margin-left: auto;
+.nav-bar {
+  display: flex;
+  align-self: stretch;
 }
 
-a:any-link {
+.nav-link:any-link {
   font-size: 20px;
   text-decoration: none;
 
   color: #000;
 
-  /* height: 100%; */
   padding: 16px 25px;
 }
 
-a:hover {
+.nav-link:hover {
   background-color: #70c3ed;
 }
 </style>
